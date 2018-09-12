982e9516a9d8c7c6505d18a67e7a8dfc05fda9ac
jonesus <joonas.palosuo@gmail.com>
2018-09-12 10:15:44 +0300

    Ex 1.6

---
+++ osa1/ex02/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(e) {
+    const target = e.target.id
+    this.setState({[target]: this.state[target] + 1})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>anna palautetta</h1>
+        <div>
+          <button onClick={this.handleOnClick} id="good">hyvä</button>
+          <button onClick={this.handleOnClick} id="neutral">neutraali</button>
+          <button onClick={this.handleOnClick} id="bad">huono</button>
+        </div>
+
+        <h2>statistiikka</h2>
+        <div>
+          {`hyvä ${this.state.good}`}
+        </div>
+        <div>
+          {`neutraali ${this.state.neutral}`}
+        </div>
+        <div>
+          {`huono ${this.state.bad}`}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
