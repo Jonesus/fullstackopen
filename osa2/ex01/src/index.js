2accabd36bd8eee3fedfe715f500583b034cef21
jonesus <joonas.palosuo@gmail.com>
2018-09-20 14:51:24 +0300

    Ex. 2.4

--- osa2/ex01/src/index.js
+++ osa2/ex01/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Otsikko = ({kurssi}) => (
-  <h1>{kurssi}</h1>
+  <h2>{kurssi}</h2>
 );
 
 const Sisalto = ({osat}) => osat.map(
@@ -26,31 +26,52 @@ const Kurssi = ({kurssi}) => (
 );
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10,
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7,
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14,
-      },
-      {
-        nimi: 'Redux',
-        tehtavia: 7,
-      }
-    ],
-  }
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+        },
+        {
+          nimi: 'Redux',
+          tehtavia: 7,
+        }
+      ],
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    },
+  ];
 
   return (
     <div>
-      <Kurssi kurssi={kurssi} />
+      <h1>Opetusohjelma</h1>
+      {kurssit.map(kurssi =>
+        <Kurssi key={kurssi.id} kurssi={kurssi} />)}
     </div>
   )
 }
