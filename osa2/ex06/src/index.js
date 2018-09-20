ab660c4214f9b71a8a56dbefc79bd4636a3ceb7f
jonesus <joonas.palosuo@gmail.com>
2018-09-20 19:07:43 +0300

    Ex. 2.9

--- osa2/ex06/src/index.js
+++ osa2/ex06/src/index.js
@@ -6,42 +6,72 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' },
       ],
-      newName: ''
+      newName: '',
+      newNumber: '',
+      filter: '',
     }
     this.submit = this.submit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleNumberChange = this.handleNumberChange.bind(this);
+    this.handleFilterChange = this.handleFilterChange.bind(this);
   }
 
   submit(event) {
     if (this.state.persons.some(person => person.name === this.state.newName)) {
       alert('No duplicates!');
     } else {
-      this.setState({ persons: [...this.state.persons, {name: this.state.newName}]})
+      this.setState({ persons:
+        [...this.state.persons, {
+          name: this.state.newName,
+          number: this.state.newNumber,
+        }]
+      })
     }
-    this.setState({ newName: '' }) ;
+    this.setState({ newName: '', newNumber: '' }) ;
     event.preventDefault();
   }
 
-  handleChange(event) {
+  handleNameChange(event) {
     this.setState({ newName: event.target.value });
+  }
+
+  handleNumberChange(event) {
+    this.setState({ newNumber: event.target.value });
+  }
+
+  handleFilterChange(event) {
+    this.setState({ filter: event.target.value });
   }
 
   render() {
     return (
       <div>
-        <h2>Puhelinluettelo</h2>
+        <h1>Puhelinluettelo</h1>
+        rajaa näytettäviä <input value={this.state.filter} onChange={this.handleFilterChange}/>
+
+        <h2>Lisää uusi</h2>
         <form onSubmit={this.submit}>
           <div>
-            nimi: <input value={this.state.newName} onChange={this.handleChange}/>
+            nimi: <input value={this.state.newName} onChange={this.handleNameChange}/>
+          </div>
+          <div>
+            numero: <input value={this.state.newNumber} onChange={this.handleNumberChange}/>
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
+
         <h2>Numerot</h2>
-        {this.state.persons.map(person => <div key={person.name}>{person.name}</div>)}
+        {this.state.persons.map(person =>
+          person.name.toLowerCase().includes(this.state.filter.toLowerCase()) &&
+            <div key={person.name}>{person.name} {person.number}</div>
+          )}
       </div>
     )
   }
