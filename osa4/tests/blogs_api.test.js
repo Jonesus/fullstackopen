652134cdd7d83facf08bbc47616e42d0998bbbf8
jonesus <joonas.palosuo@gmail.com>
2018-12-02 19:43:27 +0200

    Ex. 4.12.

--- osa4/tests/blogs_api.test.js
+++ osa4/tests/blogs_api.test.js
@@ -1,66 +1,89 @@
 const supertest = require('supertest')
 const { app, server } = require('../index')
+const Blog = require('../models/blog')
+const helpers = require('./test_helper')
 const api = supertest(app)
 
 describe('blogs', () => {
-  test('GET returns json', async () => {
-    await api
+  beforeAll(async () => {
+    await Blog.remove({})
+
+    const blogs = await helpers.initialData.map(blog => new Blog(blog))
+    await Promise.all(blogs.map(blog => blog.save()))
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
+  test('GET returns all blogs in json', async () => {
+    const res = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+
+    expect(res.body.length).toBe(helpers.initialData.length)
   })
 
   test('POST accepts new items', async () => {
     const title = Math.random().toString(36).substring(2, 15)
-    const resp = await api
-      .post('/api/blogs')
-      .send({
-        'title': title,
-        'author': 'testboi',
-        'url': 'www.test.com',
-        'likes': 7
-      })
+    const newBlog = {
+      'title': title,
+      'author': 'testboi',
+      'url': 'www.test.com',
+      'likes': 7
+    }
 
-      expect(resp.status).toBe(201)
-      expect(resp.body.title).toBe(title)
+    const blogsBefore = await helpers.getBlogsInDb()
+    const res = await api
+      .post('/api/blogs')
+      .send(newBlog)
+    const blogsAfter = await helpers.getBlogsInDb()
+
+    expect(res.status).toBe(201)
+    expect(res.body.title).toBe(title)
+    expect(blogsAfter.length).toBe(blogsBefore.length + 1)
+    expect(blogsAfter).toContainEqual(newBlog)
   })
 
   test('POST body without likes gets set to 0', async () => {
-    const resp = await api
-      .post('/api/blogs')
-      .send({
-        'title': 'likeless',
-        'author': 'testboi',
-        'url': 'www.test.com',
-      })
+    const likelessBlog = {
+      'title': 'likeless',
+      'author': 'testboi',
+      'url': 'www.test.com',
+    }
 
-    expect(resp.status).toBe(201)
-    expect(resp.body.likes).toBe(0)
+    const blogsBefore = await helpers.getBlogsInDb()
+    const res = await api
+      .post('/api/blogs')
+      .send(likelessBlog)
+    const blogsAfter = await helpers.getBlogsInDb()
+
+    expect(res.status).toBe(201)
+    expect(res.body.likes).toBe(0)
+    expect(blogsAfter.length).toBe(blogsBefore.length + 1)
+    expect(blogsAfter).toContainEqual({...likelessBlog, likes: 0})
   })
 
   test('POST body requires title', async () => {
-    const resp = await api
+    const brokenBlog = {
+      'author': 'testboi',
+      'url': 'www.test.com',
+    }
+    await api
       .post('/api/blogs')
-      .send({
-        'author': 'testboi',
-        'url': 'www.test.com',
-      })
-
-    expect(resp.status).toBe(400)
+      .send(brokenBlog)
+      expect(400)
   })
 
   test('POST body requires url', async () => {
-    const resp = await api
+    const brokenBlog = {
+      'title': 'lollers',
+      'author': 'testboi',
+    }
+    await api
       .post('/api/blogs')
-      .send({
-        'title': 'lollers',
-        'author': 'testboi',
-      })
-
-    expect(resp.status).toBe(400)
+      .send(brokenBlog)
+      .expect(400)
   })
-})
-
-afterAll(() => {
-  server.close()
 })
---
+++ osa4/tests/test_helper.js
@@ -0,0 +1,38 @@
+const Blog = require('../models/blog')
+
+const initialData = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+]
+
+const formatBlog = (blog) => ({
+  title: blog.title,
+  author: blog.author,
+  url: blog.url,
+  likes: blog.likes
+})
+
+const getBlogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(formatBlog)
+}
+
+module.exports = {
+  initialData, formatBlog, getBlogsInDb
+}
