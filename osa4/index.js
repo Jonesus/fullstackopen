7fb440dabb2452bfb0d6d49f6dcaba53830ba182
jonesus <joonas.palosuo@gmail.com>
2018-12-02 18:14:28 +0200

    Ex. 4.8

--- osa4/index.js
+++ osa4/index.js
@@ -5,17 +5,25 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
 app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 
-const mongoUrl = 'mongodb://ebin:nice123@ds123584.mlab.com:23584/blog'
-mongoose.connect(mongoUrl)
+mongoose.connect(config.mongoUrl)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
 
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
