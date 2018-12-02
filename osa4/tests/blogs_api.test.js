bbec7728e4f24ea46b77572e5d72c20bc361de4b
jonesus <joonas.palosuo@gmail.com>
2018-12-02 18:41:56 +0200

    Ex. 4.9

--- osa4/tests/blogs_api.test.js
+++ osa4/tests/blogs_api.test.js
@@ -2,11 +2,27 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('blogs', () => {
+  test('GET returns json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('POST accepts new items', async () => {
+    const title = Math.random().toString(36).substring(2, 15)
+    await api
+      .post('/api/blogs')
+      .send({
+        'title': title,
+        'author': 'testboi',
+        'url': 'www.test.com',
+        'likes': 7
+      })
+      .expect(201)
+      .expect(res => res.body.title === title)
+  })
 })
 
 afterAll(() => {
