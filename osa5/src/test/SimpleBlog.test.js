0bc40b9ff301c6ebaa11a64275ac9777a9719ee5
jonesus <joonas.palosuo@gmail.com>
2018-12-05 14:32:05 +0200

    Ex. 5.13

--- osa5/src/test/SimpleBlog.test.js
+++ osa5/src/test/SimpleBlog.test.js
@@ -17,4 +17,14 @@ describe('<SimpleBlog />', () => {
     expect(titleDiv.text()).toContain(`${blog.title} ${blog.author}`)
     expect(likesDiv.text()).toContain(`blog has ${blog.likes} likes`)
   })
+
+  test('onClick prop gets called correctly', () => {
+    const mockFn = jest.fn()
+
+    const blogComponent = shallow(<SimpleBlog blog={{}} onClick={mockFn} />)
+    const likeButton = blogComponent.find('button')
+    likeButton.simulate('click')
+    likeButton.simulate('click')
+    expect(mockFn.mock.calls.length).toEqual(2)
+  })
 })
